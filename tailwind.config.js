b76d5498d2c7e2f62de82e986c5f85ab3f29b550
trane7776 <izbasarov.t@edu.rea.ru>
2023-08-03 13:19:41 +0300

    tailwind css news site created

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        layout: "auto 1fr auto",
+      },
+      colors: {
+        "logo-main": "#9b0404",
+      },
+      fontFamily: {
+        main: ["Montserrat"],
+      },
+    },
+  },
+  plugins: [],
+};
